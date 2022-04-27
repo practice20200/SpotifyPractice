36e7b549b0ef576fa395f0ab55699b31de0b94e9
practice20200 <wor9hard2018@gmail.com>
2022-04-26 20:14:00 -0700

    Modified Manager

---
+++ SpotifyPractice/Utils/Monagers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  SpotifyPractice
+//
+//  Created by Apple New on 2022-04-26.
+//
+
+import Foundation
---
+++ SpotifyPractice/Utils/Monagers/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  SpotifyPractice
+//
+//  Created by Apple New on 2022-04-26.
+//
+
+import Foundation
