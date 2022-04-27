af9c22c7f151f14a7b64ea5a5c85568abb4ef369
practice20200 <wor9hard2018@gmail.com>
2022-04-26 22:17:26 -0700

    Fixed TabBar'

--- SpotifyPractice/Controllers/Core/TabBarViewCollectionViewCell.swift
+++ SpotifyPractice/Controllers/Core/TabBarViewCollectionViewCell.swift
@@ -11,7 +11,6 @@ class TabBarViewController: UITabBarController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray
         
         let homeVC = HomeViewController()
         let searchVC = SearchViewController()
@@ -28,6 +27,10 @@ class TabBarViewController: UITabBarController {
         let navVC1 = UINavigationController(rootViewController: homeVC)
         let navVC2 = UINavigationController(rootViewController: searchVC)
         let navVC3 = UINavigationController(rootViewController: libraryVC)
+        
+        navVC1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        navVC2.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        navVC3.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 1)
         
         navVC1.navigationBar.prefersLargeTitles = true
         navVC2.navigationBar.prefersLargeTitles = true
