e334cd2828b944d007ca1ce8a40038b0ed54a165
practice20200 <wor9hard2018@gmail.com>
2022-04-28 10:30:32 -0700

    Created spotify

---
+++ SpotifyPractice/Utils/Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Extension.swift
+//  SpotifyPractice
+//
+//  Created by Apple New on 2022-04-28.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+}
