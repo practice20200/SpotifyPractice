bdbc907bdd2dad3f899eb996e986fd8594d91f24
practice20200 <wor9hard2018@gmail.com>
2022-05-02 10:20:37 -0700

    Fixed some errors in AuthManager

--- SpotifyPractice/Utils/Monagers/APICaller.swift
+++ SpotifyPractice/Utils/Monagers/APICaller.swift
@@ -33,10 +33,8 @@ struct APICaller {
                     completion(.failure(error))
                 }
             }
-            
-            
+            task.resume()
         }
-        
     }
     
     enum HTTPMethod: String{
